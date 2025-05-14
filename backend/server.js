77f0b0dffd1c3bc7da758103857b74b5b85c712a
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-05-14 13:38:51 +0500

    feat: frontend setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -14,6 +16,12 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 
 // middlewares
+app.use(
+  cors({
+    origin: `http://localhost:5173`,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router";
+
+import Layout from "./components/layout/Layout";
+import SignUpPage from "./pages/auth/SignUpPage";
+import LoginPage from "./pages/auth/LoginPage";
+import HomePage from "./pages/HomePage";
+
+const App = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Layout>
+  );
+};
+
+export default App;
