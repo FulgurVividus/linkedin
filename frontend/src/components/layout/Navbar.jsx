713c2109b860b3ba04ca24a661ea8ca31d846d82
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-05-17 22:55:21 +0500

    fix: useQuery

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -8,9 +8,6 @@ const Navbar = () => {
   // get the user by the queryKey
   const { data: authUser } = useQuery({
     queryKey: ["authUser"],
-    queryFn: () => {
-      console.error = () => {};
-    },
   });
 
   const queryClient = useQueryClient();
