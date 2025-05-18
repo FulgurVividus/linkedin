b871c242aa4e4838fe530c504a58420a56ecd8ad
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-05-18 17:12:21 +0500

    build: prep for prod

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -14,14 +15,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 9000;
+const __dirname = path.resolve();
 
 // middlewares
-app.use(
-  cors({
-    origin: `http://localhost:5173`,
-    credentials: true,
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: `http://localhost:5173`,
+      credentials: true,
+    })
+  );
+}
+
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
@@ -31,6 +36,14 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Serve is running on port ${PORT}`);
